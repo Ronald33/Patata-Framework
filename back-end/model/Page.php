470f11cef001ae2f9f3830033453857e19801ddf
Ronald <Ronald.dev1@gmail.com>
2018-10-15 13:31:35 -0500

    Adding DAO to extras

--- back-end/model/Page.php
+++ back-end/model/Page.php
@@ -1,7 +1,4 @@
 <?php
-require_once(LIBRARIES . 'DB/DB.php');
-use DB\DB;
-
 class Page
 {
 	public function getMessage()
--- back-end/model/Render.php
+++ back-end/model/Render.php
@@ -1,5 +1,4 @@
 <?php
-
 class Render
 {
     public static function getBooks()
